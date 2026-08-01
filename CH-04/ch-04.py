3d9fd88a1f519de390a098d868c5ffaf447b04ec
mohit-8516 <mohitshrivastav429@gmail.com>
2026-08-01 09:27:53 +0530

    coffee sales data

---
+++ CH-04/ch-04.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+st.title("☕ Coffee Sales Dashboard")
+
+file = st.file_uploader("Upload your Coffee CSV file", type=["csv"])
+
+if file:
+    df = pd.read_csv(file)
+
+    st.subheader("📋 Data Preview")
+    st.dataframe(df)
+
+    st.subheader("📊 Summary Statistics")
+    st.write(df.describe())
+
+    cities = df["City"].unique()
+    selected_city = st.selectbox("Filter by City", cities)
+
+    filtered_data = df[df["City"] == selected_city]
+
+    st.subheader(f"☕ Coffee Sales in {selected_city}")
+    st.dataframe(filtered_data)
